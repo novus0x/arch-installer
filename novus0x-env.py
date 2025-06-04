ea678651d42f836d530bf2e6600a814c25323239
novus0x <novus0x@proton.me>
2025-06-04 09:17:47 +0000

    Test 07

--- novus0x-env.py
+++ novus0x-env.py
@@ -6,7 +6,7 @@ from termcolor import colored, cprint
 # Show banner
 banner = subprocess.run(["toilet", "-f", "smblock", "Dotfiles Installer\n\t\tby Novus0x\n"], capture_output=True, text=True)
 
-# Packages
+# Config
 packages = ["xorg", "xorg-server", "xorg-xinit", "xorg-xrandr", "xorg-xsetroot", "mesa", "lightdm", "lightdm-gtk-greeter", "neovim", "i3", "i3-gaps", "i3lock", "polybar", "picom", "rofi", "pulseaudio", "playerctl", "feh", "net-tools", "upower", "bat", "lsd", "lua-language-server", "go", "ttf-fira-code", "ttf-jetbrains-mono-nerd", "ttf-dejavu", "ttf-font-awesome"]
 
 # Main function
@@ -16,11 +16,16 @@ def main():
 
     cprint("[+] Updating system", "green")
     subprocess.run(["sudo", "pacman", "-Syu", "--noconfirm"])
+    cprint("[+] Installing dependencies", "green")
     subprocess.run(["sudo", "pacman", "-S", "--needed", "--noconfirm"] + packages)
+    cprint("[+] Installing yay", "green")
     subprocess.run(["git", "clone", "https://aur.archlinux.org/yay.git"])
-    subprocess.run("cd yay && makepkg -si --noconfirm", shell=True))
-
+    subprocess.run("cd yay && makepkg -si --noconfirm", shell=True)
+    cprint("[+] Enabling upower and lightdm", "green")
     subprocess.run(["sudo", "systemctl", "enable", "upower.service", "lightdm.service"])
-    
+    cprint("[+] Adding lightdm configurations", "green")
+    subprocess.run(["sudo", "cp", "-r", "./config/lightdm/lightdm.conf", "/etc/lightdm/lightdm.conf"])
+    subprocess.run(["sudo", "cp", "-r", "./config/lightdm/lightdm-gtk-greeter.conf", "/etc/lightdm/lightdm-gtk-greeter.conf"])
+
 if __name__ == "__main__":
     main()
